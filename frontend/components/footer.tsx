b1275ebfa416a6348f2a318a6945bbedcfff8dfa
pxeger <_@pxeger.com>
2021-04-05 13:52:30 +0100

    add space in copyright

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
     <footer className="px-4 py-2 bg-black bg-opacity-5 dark:bg-opacity-20 absolute bottom-0 left-0 right-0 flex justify-between">
       <span>
         ©
+        {' '}
         {new Date().getUTCFullYear()}
         {' '}
         Patrick Reader and contributors
