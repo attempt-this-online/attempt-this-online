6d4fd3b73fdfd74c15c91e5a1eabb978e392fdd5
pxeger <_@pxeger.com>
2021-04-05 12:32:01 +0100

    fix footer layout in case a scrollbar appears

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="px-4 py-2 w-screen bg-black bg-opacity-5 dark:bg-opacity-20 absolute bottom-0">
+    <footer className="px-4 py-2 bg-black bg-opacity-5 dark:bg-opacity-20 absolute bottom-0 left-0 right-0 flex justify-between">
       <span>
         ©
         {new Date().getUTCFullYear()}
         {' '}
         Patrick Reader and contributors
       </span>
-      <a href="https://github.com/pxeger/attempt_this_online" className="float-right">{buildId}</a>
+      <a href="https://github.com/pxeger/attempt_this_online">{buildId}</a>
     </footer>
   );
 }
