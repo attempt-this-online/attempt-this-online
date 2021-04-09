67caefc2ced3d155fd266acdd5d30845039356d1
pxeger <_@pxeger.com>
2021-04-09 16:07:20 +0100

    link to licence in footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -5,13 +5,13 @@ export default function Footer({ noLegalLink = false }: { noLegalLink?: boolean 
   return (
     <footer className="px-4 py-2 bg-black bg-opacity-5 dark:bg-opacity-20 w-100 flex flex-col text-center md:flex-row">
       <div className="order-2 md:order-1 md:flex md:flex-grow md:justify-center md:w-0">
-        <span className="mr-auto">
+        <a href="https://github.com/pxeger/attempt_this_online#licence" className="mr-auto">
           ©
           {' '}
           {new Date().getUTCFullYear()}
           {' '}
           Patrick Reader and contributors
-        </span>
+        </a>
       </div>
       <div className="order-3 md:order-2 md:flex md:flex-grow md:justify-center md:w-0">
         { noLegalLink ? null : <Link href="/legal"><a className="underline text-blue-500">Legal</a></Link>}
