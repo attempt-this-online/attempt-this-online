16f298c19eca718fd78d618b9f0a18fa812fb272
Patrick Reader <_@pxeger.com>
2021-11-06 17:13:08 +0000

    add all the languages to new backend
    tbqh, dynamically gathering the list of languages was overkill

--- backend/languages.go
+++ backend/languages.go
@@ -22,6 +22,14 @@ func init() {
 }
 
 var languages = map[string]language{
+	"whython": {
+		Name:     "Whython",
+		Image:    "attemptthisonline/whython",
+		Version:  "Latest",
+		Url:      "https://github.com/pxeger/whython",
+		Sbcs:     false,
+		SE_class: "python",
+	},
 	"python": {
 		Name:     "Python",
 		Image:    "attemptthisonline/python",
@@ -29,5 +37,58 @@ var languages = map[string]language{
 		Url:      "https://www.python.org",
 		Sbcs:     false,
 		SE_class: "python",
+	},
+	"zsh": {
+		Name:     "Zsh",
+		Image:    "attemptthisonline/zsh",
+		Version:  "5",
+		Url:      "https://www.zsh.org/",
+		Sbcs:     false,
+		SE_class: "bash",
+	},
+	"jelly": {
+		Name:    "Jelly",
+		Image:   "attemptthisonline/jelly",
+		Version: "70c9fd93",
+		Url:     "https://github.com/DennisMitchell/jellylanguage",
+		Sbcs:    true,
+	},
+	"ruby": {
+		Name:     "Ruby",
+		Image:    "attemptthisonline/ruby",
+		Version:  "Latest",
+		Url:      "https://www.ruby-lang.org/",
+		Sbcs:     false,
+		SE_class: "ruby",
+	},
+	"python2": {
+		Name:     "Python 2",
+		Image:    "attemptthisonline/python2",
+		Version:  "2",
+		Url:      "https://docs.python.org/2/",
+		Sbcs:     false,
+		SE_class: "python2",
+	},
+	"scala3": {
+		Name:    "Scala 3",
+		Image:   "attemptthisonline/scala3",
+		Version: "3",
+		Url:     "https://www.scala-lang.org/",
+		Sbcs:    false,
+	},
+	"scala2": {
+		Name:    "Scala 2",
+		Image:   "attemptthisonline/scala2",
+		Version: "2",
+		Url:     "https://www.scala-lang.org/",
+		Sbcs:    false,
+	},
+	"java": {
+		Name:     "Java",
+		Image:    "attemptthisonline/java",
+		Version:  "Latest",
+		Url:      "https://en.wikipedia.org/wiki/Java_(programming_language)",
+		Sbcs:     false,
+		SE_class: "java",
 	},
 }
