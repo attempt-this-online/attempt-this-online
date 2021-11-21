7b78f25ed9fc7e012eb85fcf3837b47623784005
Patrick Reader <_@pxeger.com>
2021-11-21 10:59:48 +0000

    go fmt

--- backend/server.go
+++ backend/server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "github.com/attempt-this-online/attempt-this-online/backend"
+	"github.com/attempt-this-online/attempt-this-online/backend"
 )
 
 func main() {
-    backend.ServerMain()
+	backend.ServerMain()
 }
