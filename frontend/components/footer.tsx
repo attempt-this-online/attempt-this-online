279ce148189a84d537c3ebe5c858800521141411
Patrick Reader <_@pxeger.com>
2022-02-05 19:59:20 +0000

    clean up code for responsiveness of footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Footer({ noLegalLink = false }: { noLegalLink?: boolean }) {
   return (
     <footer className="px-4 py-2 bg-black bg-opacity-5 dark:bg-opacity-20 w-100 flex flex-col text-center md:flex-row">
-      <div className="order-2 md:order-1 md:flex md:grow md:justify-center md:w-0">
+      <div className="md:order-1 md:flex md:grow md:justify-center md:w-0">
         <a href="https://github.com/attempt-this-online/attempt-this-online#licence" className="mr-auto">
           ©
           {' '}
@@ -13,11 +13,11 @@ export default function Footer({ noLegalLink = false }: { noLegalLink?: boolean 
           Patrick Reader and contributors
         </a>
       </div>
-      <div className="order-3 md:order-2 md:flex md:grow md:justify-center md:w-0">
+      <div className="md:order-2 md:flex md:justify-center md:w-0">
         { noLegalLink ? null : <Link href="/legal"><a className="underline text-blue-500">Legal</a></Link>}
       </div>
-      <div className="order-1 md:order-3 md:flex md:grow md:justify-center md:w-0">
-        <div className="ml-auto md:max-w-qu truncate">
+      <div className="md:order-3 md:flex md:grow md:justify-center md:w-0">
+        <div className="ml-auto truncate">
           <a href="https://github.com/attempt-this-online/attempt-this-online">
             Version:
             {' '}
