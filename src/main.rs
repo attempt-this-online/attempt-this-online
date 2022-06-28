d7ea73b01af6a4b624aaeca9aa63ab116e867ea6
Patrick Reader <_@pxeger.com>
2022-06-28 13:29:48 +0100

    websocket hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use futures_util::SinkExt;
+use futures_util::StreamExt;
+use warp::Filter;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let execute = warp::path!("api" / "v0" / "ws" / "execute")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            ws.on_upgrade(handle_ws)
+        })
+        .with(warp::cors().allow_any_origin());
+    warp::serve(execute).run(([127, 0, 0, 1], 8500)).await;
+}
+
+async fn handle_ws(websocket: warp::ws::WebSocket) {
+    let (mut sender, _) = websocket.split();
+    sender.send(warp::ws::Message::text("Hello, World!")).await.unwrap();
 }
