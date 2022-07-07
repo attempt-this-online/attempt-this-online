bc526bea962d23e57d0df9dac17bb8c8bd930bc8
Patrick Reader <_@pxeger.com>
2022-07-07 11:33:38 +0100

    improvements

--- src/invoke.rs
+++ src/invoke.rs
@@ -1,5 +1,6 @@
 mod codes;
-use crate::codes::*;
+mod languages;
+use crate::{codes::*, languages::*};
 use serde::{Deserialize, Serialize};
 use serde_bytes::ByteBuf;
 use std::io::Write;
@@ -38,11 +39,20 @@ fn main() -> std::process::ExitCode {
             return std::process::ExitCode::from(POLICY_VIOLATION);
         }
     };
-    let _ = request; // TODO: use request
-    let encoded_output = match rmp_serde::to_vec_named(&Response {
-        stdout: ByteBuf::from(b"hello".to_vec()),
-        stderr: ByteBuf::from(b"goodbye".to_vec()),
-    }) {
+    let language = match validate(&request) {
+        Ok(l) => l,
+        Err(e) => {
+            log_error!("invalid request: {}", e);
+            return std::process::ExitCode::from(POLICY_VIOLATION);
+        }
+    };
+    let result = match invoke(&request, language) {
+        Ok(r) => r,
+        Err(_) => {
+            return std::process::ExitCode::from(INTERNAL_ERROR);
+        }
+    };
+    let encoded_output = match rmp_serde::to_vec_named(&result) {
         Ok(r) => r,
         Err(e) => {
             log_error!("error encoding output: {}", e);
@@ -56,4 +66,39 @@ fn main() -> std::process::ExitCode {
             std::process::ExitCode::from(INTERNAL_ERROR)
         }
     }
+}
+
+enum ValidationError<'a> {
+    NoSuchLanguage(&'a String),
+    NullByteInArgument,
+}
+
+impl<'a> std::fmt::Display for ValidationError<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ValidationError::NoSuchLanguage(name) => write!(f, "no such language: {}", name),
+            ValidationError::NullByteInArgument => write!(f, "argument contains null byte"),
+        }
+    }
+}
+
+fn validate(request: &Request) -> Result<&Language, ValidationError> {
+    for arg in request.options.iter().chain(request.arguments.iter()) {
+        if arg.contains(&0) {
+            return Err(ValidationError::NullByteInArgument);
+        }
+    }
+    if let Some(l) = LANGUAGES.get(&request.language) {
+        Ok(l)
+    } else {
+        Err(ValidationError::NoSuchLanguage(&request.language))
+    }
+}
+
+fn invoke(request: &Request, language: &Language) -> Result<Response, std::convert::Infallible> {
+    let _ = (request, language);
+    Ok(Response {
+        stdout: ByteBuf::from(b"hello".to_vec()),
+        stderr: ByteBuf::from(b"goodbye".to_vec()),
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+extern crate lazy_static;
+
 mod codes;
 use crate::codes::*;
-use futures_util::stream::SplitSink;
 use futures_util::SinkExt;
 use futures_util::StreamExt;
 use std::process::Stdio;
@@ -26,24 +27,29 @@ async fn handle_ws(websocket: WebSocket) {
         let message = match received {
             Ok(r) => r,
             Err(e) => {
-                // TODO: handle error
                 eprintln!("error reading from websocket: {}", e);
                 continue;
             }
         };
         if message.is_close() {
-            if let Err(e) = receiver.reunite(sender).unwrap().close().await {
-                eprintln!("error closing websocket: {}", e);
-            }
             break;
         }
         let response = match invoke(message.as_bytes()).await {
             Ok(r) => r,
             Err((code, e)) => {
-                let msg = format!("internal error: {}", e);
-                eprintln!("{}", msg);
-                handle_ws_error(&mut sender, msg, (code as u16) + WEBSOCKET_BASE).await;
-                continue;
+                let msg = if code == POLICY_VIOLATION {
+                    e
+                } else {
+                    format!("internal error: {}", e)
+                };
+                if let Err(e) = sender
+                    .send(Message::close_with(WEBSOCKET_BASE + code as u16, msg))
+                    .await
+                {
+                    // can't do anything but log it
+                    eprintln!("error sending close code: {}", e);
+                }
+                return;
             }
         };
         match sender.send(Message::binary(response)).await {
@@ -51,18 +57,11 @@ async fn handle_ws(websocket: WebSocket) {
             Err(e) => {
                 eprintln!("error sending to websocket: {}", e);
             }
-        };
-    };
-}
-
-async fn handle_ws_error(
-    sender: &mut SplitSink<WebSocket, Message>,
-    error: String,
-    code: u16,
-) {
-    if let Err(e) = sender.send(Message::close_with(code, error)).await {
+        }
+    }
+    if let Err(e) = sender.close().await {
         // can't do anything but log it
-        eprintln!("error sending close code: {}", e);
+        eprintln!("error closing websocket: {}", e);
     }
 }
 
@@ -96,13 +95,15 @@ async fn invoke(input: &[u8]) -> Result<Vec<u8>, (u8, String)> {
         }
     };
     if !output.status.success() {
-        Err((
-            output.status.code().unwrap_or(11) as u8,
-            format!(
-                "error running ATO_invoke:\n{}",
-                std::string::String::from_utf8_lossy(&output.stdout[..]),
-            ),
-        ))
+        let msg = std::string::String::from_utf8_lossy(&output.stdout[..]);
+        let code = match output.status.code() {
+            Some(c) => c as u8,
+            None => {
+                eprintln!("error running ATO_invoke: {}", output.status);
+                INTERNAL_ERROR
+            }
+        };
+        Err((code, msg.into()))
     } else {
         Ok(output.stdout)
     }
