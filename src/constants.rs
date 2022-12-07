b917e28aae721133b837d6cc295842006a223843
pxeger <_@pxeger.com>
2022-12-07 15:06:57 +0000

    make max request size definition more readable

--- src/constants.rs
+++ src/constants.rs
@@ -5,11 +5,11 @@ pub const NORMAL: u8 = 0;
 pub const UNSUPPORTED_DATA: u8 = 3;
 pub const POLICY_VIOLATION: u8 = 8;
 pub const INTERNAL_ERROR: u8 = 11;
-pub const MAX_REQUEST_SIZE: usize = 1 << 16; // 64KiB
 #[allow(non_upper_case_globals)]
 pub const KiB: u64 = 1024;
 #[allow(non_upper_case_globals)]
 pub const MiB: u64 = KiB * KiB;
+pub const MAX_REQUEST_SIZE: usize = 64 * KiB as usize;
 
 pub const PIPE_BUF: usize = 4096; // from limits.h
 pub const OUTPUT_BUF_SIZE: usize = PIPE_BUF - 256; // subtract enough to account for msgpack overhead
